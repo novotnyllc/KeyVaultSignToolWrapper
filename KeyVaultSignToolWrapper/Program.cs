732b4e5edd6b7939d3c1fedd9b976825f2419da8
Oren Novotny <oren@novotny.org>
2017-07-30 21:41:32 -0400

    wrap signtool

--- KeyVaultSignToolWrapper/Program.cs
+++ KeyVaultSignToolWrapper/Program.cs
@@ -1,12 +1,77 @@
-﻿using System;
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace KeyVaultSignToolWrapper
 {
     class Program
     {
-        static void Main(string[] args)
+        internal static int Main(string[] args)
         {
+            var application = new CommandLineApplication(throwOnUnexpectedArg: false);
+            var signCommand = application.Command("sign", throwOnUnexpectedArg: false, configuration: signConfiguration =>
+            {
+                signConfiguration.Description = "Invokes SignTool with the specified Key Vault certificate.";
+                signConfiguration.HelpOption("-? | -h | --help");
+                
+                var signTool = signConfiguration.Argument("signtool", "Path to SignTool.");
+                var file = signConfiguration.Argument("file", "File to sign.");
+                var signToolArgs = signConfiguration.Argument("signtool-args", "Command line arguments to SignTool excluding the file.");
+                
+                var azureKeyVaultUrl = signConfiguration.Option("-kvu | --azure-key-vault-url", "The URL to an Azure Key Vault.", CommandOptionType.SingleValue);
+                var azureKeyVaultClientId = signConfiguration.Option("-kvi | --azure-key-vault-client-id", "The Client ID to authenticate to the Azure Key Vault.", CommandOptionType.SingleValue);
+                var azureKeyVaultClientSecret = signConfiguration.Option("-kvs | --azure-key-vault-client-secret", "The Client Secret to authenticate to the Azure Key Vault.", CommandOptionType.SingleValue);
+                var azureKeyVaultCertificateName = signConfiguration.Option("-kvc | --azure-key-vault-certificate", "The name of the certificate in Azure Key Vault.", CommandOptionType.SingleValue);
+                var azureKeyVaultAccessToken = signConfiguration.Option("-kva | --azure-key-vault-accesstoken", "The Access Token to authenticate to the Azure Key Vault.", CommandOptionType.SingleValue);
+
+                signConfiguration.OnExecute(() =>
+                {
+                    if (string.IsNullOrWhiteSpace(signTool.Value) || string.IsNullOrWhiteSpace(file.Value) || string.IsNullOrWhiteSpace(signToolArgs.Value))
+                    {
+                        application.Error.WriteLine("All arguments are required");
+                        return Task.FromResult(-1);
+                    }
+
+                    if (!azureKeyVaultUrl.HasValue())
+                    {
+                        application.Error.WriteLine("Key Vault URL not specified");
+                        return Task.FromResult(-1);
+                    }
+
+                    if (!azureKeyVaultCertificateName.HasValue())
+                    {
+                        application.Error.WriteLine("Certificate name not specified");
+                        return Task.FromResult(-1);
+                    }
+                    
+                    var valid = (azureKeyVaultAccessToken.HasValue() || (azureKeyVaultClientId.HasValue() && azureKeyVaultClientSecret.HasValue()));
+                    if (!valid)
+                    {
+                        application.Error.WriteLine("Either access token or clientId and client secret must be specified");
+                        return Task.FromResult(-1);
+                    }
+
+                    var cmd = new SignCommand(application);
+                    return cmd.SignAsync(signTool.Value,
+                                         signToolArgs.Value,
+                                         file.Value,
+                                         azureKeyVaultCertificateName.Value(),
+                                         azureKeyVaultUrl.Value(),
+                                         azureKeyVaultClientId.Value(),
+                                         azureKeyVaultClientSecret.Value(),
+                                         azureKeyVaultAccessToken.Value());  
+                });
+            }
+            );
             
+            application.HelpOption("-? | -h | --help");
+            application.VersionOption("-v | --version", typeof(Program).Assembly.GetName().Version.ToString(3));
+            if (args.Length == 0 || args[1] != "sign")
+            {
+                application.ShowHelp();
+            }
+            return application.Execute(args);
         }
     }
 }
---
+++ KeyVaultSignToolWrapper/SignCommand.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Extensions.CommandLineUtils;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace KeyVaultSignToolWrapper
+{
+    class SignCommand
+    {
+        readonly CommandLineApplication application;
+
+        public SignCommand(CommandLineApplication application)
+        {
+            this.application = application;
+        }
+        
+        public async Task<int> SignAsync(string signTool,
+                                         string signToolArgs,
+                                         string file,
+                                         string keyVaultCertificateName,
+                                         string keyVaultUrl,
+                                         string keyVaultClientId,
+                                         string keyVaultClientSecret,
+                                         string keyVaultAccessToken)
+        {
+            /* We need to do a few things here.
+             * 
+             * 1. Get an access token if one is not provided. If one is provided, we assume it's valid.
+             * 2. Download and save the certificate. We do this each time as a way to verify credentials and the certificate name
+             * 3. Pass the location of the cert to SignTool via the -f parameter.
+             * 4. Pass the file to sign for SignTool 
+             * 
+             */
+
+            string validatedToken = null;
+            
+            async Task<string> Authenticate(string authority, string resource, string scope)
+            {
+                if (!string.IsNullOrWhiteSpace(keyVaultAccessToken))
+                {
+                    validatedToken = keyVaultAccessToken;
+                    return keyVaultAccessToken;
+                }
+
+                var context = new AuthenticationContext(authority);
+                var credential = new ClientCredential(keyVaultClientId, keyVaultClientSecret);
+
+                var result = await context.AcquireTokenAsync(resource, credential).ConfigureAwait(false);
+                if (result == null)
+                {
+                    throw new InvalidOperationException("Authentication to Azure failed.");
+                }
+                validatedToken = result.AccessToken;
+                return result.AccessToken;
+            }
+
+            var client = new KeyVaultClient(Authenticate, new HttpClient());
+
+            // We call this here to verify it's a valid cert
+            // It also implicitly validates the access token or credentials
+            var kvcert = await client.GetCertificateAsync(keyVaultUrl, keyVaultCertificateName).ConfigureAwait(false);
+
+            var fileName = Path.Combine(Environment.GetEnvironmentVariable("TEMP"), "KeyVaultCerts", $"{keyVaultCertificateName}.cer");
+            File.WriteAllBytes(fileName, kvcert.Cer);
+
+            signToolArgs += $@"/f ""{fileName}"" {file} ";
+            
+            var psi = new ProcessStartInfo
+            {
+                FileName = signTool,
+                Arguments = signToolArgs,
+                CreateNoWindow = true,
+                Environment =
+                {
+                    { "KEYVAULT_ACCESSTOKEN", validatedToken },
+                    { "KEYVAULT_URL", keyVaultUrl },
+                    { "KEYVAULT_CERTIFICATE_NAME", keyVaultCertificateName },
+                },
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+
+            var process = Process.Start(psi);
+
+            var output = process.StandardOutput.ReadToEnd();
+            var error = process.StandardOutput.ReadToEnd();
+
+            application.Out.Write(output);
+            application.Error.WriteLine(error);
+            process.WaitForExit();
+
+            return process.ExitCode;
+        }
+    }
+}
