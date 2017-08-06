bdf9aeefc5092c809ff65331cf3f5a0094673bf9
Oren Novotny <oren@novotny.org>
2017-08-05 23:01:56 -0400

    Simplify by using X509Certificate2

--- KeyVaultSigner/Exports.cs
+++ KeyVaultSigner/Exports.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using KeyVaultSigner.Win32;
 
 namespace KeyVaultSigner
@@ -9,17 +10,17 @@ namespace KeyVaultSigner
     public static class Exports
     {
         [DllExport("AuthenticodeDigestSign", CallingConvention.Winapi)]
-        public static uint AuthenticodeDigestSign([In] ref CERT_CONTEXT pSignerCert,
-                                                     [In] ref CRYPT_ATTR_BLOB pMetadataBlob,
+        public static uint AuthenticodeDigestSign([In] IntPtr pSignerCert,
+                                                     [In] CRYPT_ATTR_BLOB pMetadataBlob,
                                                      [In] AlgId digestAlgID,
                                                      [In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 4)] byte[] pbToBeSignedDigest,
                                                      [In] int cbToBeSignedDigest,
                                                      [Out] out CRYPT_ATTR_BLOB pSignedDigest
                                                      )
         {
-           
 
-            
+
+            var signerCert = new X509Certificate2(pSignerCert);
 
             pSignedDigest = default;
             return 0;
